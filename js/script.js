58ccaf62221bab07d839917a108e78a3ee2e83d0
Jakub <5243212@gmail.com>
2021-01-05 11:27:11 +0300

    First commit

---
+++ js/script.js
@@ -0,0 +1,19 @@
+"use strict";
+
+const category = 'toys';
+
+console.log(`https://someurl.com/${category}/5`); 
+
+let incr = 10,
+decr = 10 ;
+
+// ++incr;
+// --decr;
+
+console.log(incr++);
+console.log(decr--);
+
+const isChecked = true,
+      isClosed = false;
+
+console.log(!isChecked || isClosed);
